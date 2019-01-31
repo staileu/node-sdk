3fec102ca55fb2fe046d3024352ade990e94e650
Le_Futuriste <spamfree@matthieubessat.fr>
2019-01-31 07:38:59 +0100

    feat: add getAuthorizeUrl() method

--- src/Client.ts
+++ src/Client.ts
@@ -1,6 +1,39 @@
 export default class Client {
-    public isWorking(): boolean
-    {
-        return true
+    private readonly appId: String = '';
+
+    private readonly appSecret: String = '';
+
+    public readonly scope = {
+        readProfile: 'read-profile',
+        readEmail: 'read-email',
+        readBirthday: 'read-birthday',
+        readRealName: 'read-real-name'
+    };
+
+    // private readonly apiEndpoint = 'https://api-v2.stail.eu';
+
+    private readonly userEndpoint = 'https://user.stail.eu';
+
+    public constructor(appId: String, appSecret: String) {
+        this.appId = appId;
+        this.appSecret = appSecret;
+    }
+
+    public getAppId(): String {
+        return this.appId
+    }
+
+    public getAppSecret(): String {
+        return this.appSecret
+    }
+
+    /**
+     * Get the STAIL.EU authorize url to redirect the user on.
+     *
+     * @param redirectUri
+     * @param scope
+     */
+    public getAuthorizeUrl(redirectUri: String, scope: String[] = [this.scope.readProfile]): string {
+        return this.userEndpoint + '/authorize?client_id=' + this.appId + '&scope=' + scope.join('%20') + '&redirect_uri=' + redirectUri
     }
 }
--- test/client.test.ts
+++ test/client.test.ts
@@ -1,8 +1,27 @@
 import Client from '../src/Client'
 
+let config = {
+    id: process.env.APP_ID === undefined ? '' : process.env.APP_ID,
+    secret: process.env.APP_SECRET === undefined ? '' : process.env.APP_SECRET,
+    redirectUri: 'https://example.org/authorize'
+};
+
 describe('client', () => {
-  let client = new Client();
-  test('client is working', () => {
-    expect(client.isWorking()).toBe(true)
-  })
+    let client = new Client(config.id, config.secret);
+    it('should return app id and app secret correctly', () => {
+        expect(client.getAppId()).toBe(config.id);
+        expect(client.getAppSecret()).toBe(config.secret)
+    });
+    it('should return login url', () => {
+        let authorizeUrl = client.getAuthorizeUrl(config.redirectUri, [
+            client.scope.readProfile,
+            client.scope.readEmail
+        ]);
+        expect(authorizeUrl).toBeDefined();
+        expect(authorizeUrl)
+            .toBe(
+                'https://user.stail.eu/authorize?client_id=' + config.id + '&scope=read-profile%20read-email&redirect_uri=' + config.redirectUri
+            );
+        console.log(authorizeUrl)
+    })
 });
