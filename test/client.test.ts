3e846c3537fd8e004ce6b25c9fe424db77ab3f7e
Le_Futuriste <spamfree@matthieubessat.fr>
2019-06-22 15:18:31 +0200

    test: better test ux

--- test/client.test.ts
+++ test/client.test.ts
@@ -20,40 +20,45 @@ describe('client', () => {
         expect(client.getAppId()).toBe(config.id);
         expect(client.getAppSecret()).toBe(config.secret)
     });
-    it('should return login url', () => {
-        let authorizeUrl = client.getAuthorizeUrl(config.redirectUri, [
-            client.scope.readProfile,
-            client.scope.readEmail
-        ]);
-        expect(authorizeUrl).toBeDefined();
-        expect(authorizeUrl)
-            .toBe(
-                'https://user.stail.eu/authorize?client_id=' + config.id + '&scope=read-profile%20read-email&redirect_uri=' + config.redirectUri
-            );
-        console.log(authorizeUrl)
-    });
-    it('should verify the oauth code', () => {
-        let code = config.oAuthCode;
-        return client.verify(code).then(isValid => {
-            expect(isValid).toBeTruthy();
-        })
-    });
-    it('should fetch the user and return it', () => {
-        return client.fetchOAuthUser().then(user => {
-            expect(user).toBeDefined();
-            console.log(user);
+    if (config.oAuthCode === '') {
+        it('should return login url', () => {
+            let authorizeUrl = client.getAuthorizeUrl(config.redirectUri, [
+                client.scope.readProfile,
+                client.scope.readEmail
+            ]);
+            expect(authorizeUrl).toBeDefined();
+            expect(authorizeUrl)
+                .toBe(
+                    'https://user.stail.eu/authorize?client_id=' + config.id + '&scope=read-profile%20read-email&redirect_uri=' + config.redirectUri
+                );
+            console.log("authorize url received:", authorizeUrl)
+        });
+    } else {
+        it('should verify the oauth code', () => {
+            let code = config.oAuthCode;
+            return client.verify(code).then(isValid => {
+                expect(isValid).toBeTruthy();
+            })
+        });
+        it('should fetch the user and return it', () => {
+            return client.fetchOAuthUser().then(user => {
+                expect(user).toBeDefined();
+                console.log("user received: ", user);
+                expect(user.id).toBeDefined()
+                expect(user.username).toBeDefined()
+                if (user.avatar !== null) {
+                    console.log(user.avatar.getUrl())
+                }
+            })
+        });
+        it('should fetch the avatar and return base64', () => {
+            let user = client.getOAuthUser();
             if (user.avatar !== null) {
-                console.log(user.avatar.getUrl())
+                return user.avatar.getBase64(true).then(base64DataUri => {
+                    expect(base64DataUri).toBeDefined();
+                    console.log(base64DataUri)
+                })
             }
         })
-    });
-    it('should fetch the avatar and return base64', () => {
-        let user = client.getOAuthUser();
-        if (user.avatar !== null) {
-            return user.avatar.getBase64(true).then(base64DataUri => {
-                expect(base64DataUri).toBeDefined();
-                console.log(base64DataUri)
-            })
-        }
-    })
+    }
 });
